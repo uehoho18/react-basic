92d0241e69bad71bf6ecddf4a0942d08e17d7a04
uehoho18 <ramune276@gmail.com>
2022-11-30 01:32:32 +0900

    prevCount

--- src/components/Basic1.js
+++ src/components/Basic1.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Basic1 = (props) => {
   const clickHandler = () => {
     console.log('clicked')
   }
+
+const [count, setCount] = useState(0)
+
   return (
     <>
-      <button onClick={clickHandler}>Click</button>
+      <button onClick={() => {setCount(prevCount=>prevCount+1); setCount(prevCount=>prevCount+1);} }>Count {count}</button>
+
       <h1>Hello { props.name }</h1>
       <h1>Hello React2</h1>
     </>
